5245fd0fa42ff0dee8c812122925c560d726d6a9
lovebeargit <lovebearsix@163.com>
2017-07-24 22:10:38 +0800

    逗号隔开

--- TestProject/Form1.cs
+++ TestProject/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,7 +20,20 @@ namespace TestProject
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            string n = textBox1.Text;
+            char[] c = n.ToCharArray();
+            string temp = string.Empty;
+            int k = 0;
+            for (int i = n.Length-1; i >=0; i--)
+            {
+                temp+=c[k];
+                if (i % 3 == 0 && i!=0)
+                {
+                    temp += ",";
+                }
+                k++;
+            }
+            textBox1.Text = temp;
         }
     }
 }
