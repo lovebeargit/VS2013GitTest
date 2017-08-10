e1b44b1f34ce98f32c89b2b5a29d113faf666bfe
lovebeargit <lovebearsix@163.com>
2017-08-10 21:11:38 +0800

    MVC+EF

---
+++ Mycontrol/MyButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mycontrol
+{
+    public  class MyButton : UserControl
+    {
+        Label lable = null;
+        public MyButton()
+        {
+            //InitializeComponent();
+            lable = new Label();
+            lable.Location = new System.Drawing.Point(0, 0);
+            lable.Width = 60;
+            lable.Height = 30;
+            lable.Text = "Honey";
+
+            this.Controls.Add(lable);
+        }
+    }
+}
--- Mycontrol/Userpanel.cs
+++ Mycontrol/Userpanel.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TestProject
+namespace Mycontrol
 {
-    public partial class Form2 : Form
+    public partial class Userpanel : UserControl
     {
-        public Form2()
+        public Userpanel()
         {
             InitializeComponent();
-
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+        private void Userpanel_Load(object sender, EventArgs e)
         {
-            Form1 f1 = (Form1)this.Owner;
-            label1.Text += f1.name;
-            this
-        }
 
+        }
     }
 }
